c50bc4d883e9d8cd2b51b610b5ad88d29fae69ac
myTselection <myTselection@gmail.com>
2023-04-17 23:41:00 +0200

    3.0.0 first service support, implementing #6

--- custom_components/carbu_com/__init__.py
+++ custom_components/carbu_com/__init__.py
@@ -74,6 +74,8 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(config_entry, Platform.SENSOR)
     )
+    _LOGGER.info(f"{DOMAIN} register_services")
+    register_services(hass, config_entry)
     return True
 
 
@@ -83,3 +85,105 @@ async def async_remove_entry(hass, config_entry):
         _LOGGER.info("Successfully removed sensor from the integration")
     except ValueError:
         pass
+
+
+def register_services(hass, config_entry):
+        
+    async def handle_get_lowest_fuel_price(call):
+        """Handle the service call."""
+        fuel_type = call.data.get('fuel_type')
+        country = call.data.get('country')
+        postalcode = call.data.get('postalcode')
+        town = call.data.get('town','')
+        max_distance = call.data.get('max_distance')
+        filter = call.data.get('filter','')
+
+        #TODO use enum
+        super95_fueltypecode = "E10"
+        super98_fueltypecode = "SP98"
+        diesel_fueltypecode = "GO"
+        
+        session = ComponentSession()
+        carbuLocationInfo = await hass.async_add_executor_job(lambda: session.convertPostalCode(postalcode, country, town))
+        if not carbuLocationInfo:
+            raise Exception(f"Location not found country: {country}, postalcode: {postalcode}, town: {town}")
+        town = carbuLocationInfo.get("n")
+        city = carbuLocationInfo.get("pn")
+        countryname = carbuLocationInfo.get("cn")
+        locationid = carbuLocationInfo.get("id")
+        _LOGGER.debug(f"{NAME} serivce get_lowest_fuel_price convertPostalCode postalcode: {postalcode}, town: {town}, city: {city}, countryname: {countryname}, locationid: {locationid}")
+        if fuel_type == "super95":
+            price_info = await hass.async_add_executor_job(lambda: session.getFuelPrice(postalcode, country, town, locationid, super95_fueltypecode, False))
+            _LOGGER.debug(f"{NAME} price_info super95 {price_info}")
+        elif fuel_type == "super98":
+            price_info = await hass.async_add_executor_job(lambda: session.getFuelPrice(postalcode, country, town, locationid, super98_fueltypecode, False))
+            _LOGGER.debug(f"{NAME} price_info super98 {price_info}")
+        elif fuel_type == "diesel":
+            price_info = await hass.async_add_executor_job(lambda: session.getFuelPrice(postalcode, country, town, locationid, diesel_fueltypecode, False))
+            _LOGGER.debug(f"{NAME} price_info diesel {price_info}")
+        else:
+            _LOGGER.error(f"{NAME} Unsupported fuel type provided in service get_lowest_fuel_price: fuel_type: {fuel_type}, please use super95/super98/diesel")
+        
+        data = {
+            "price" : None,
+            "distance" : 0,
+            "region" : max_distance,
+            "localPrice" : 0,
+            "diff" : 0,
+            "diff30" : 0,
+            "diffPct" : 0,
+            "supplier" : None,
+            "supplier_brand" : None,
+            "url" : None,
+            "entity_picture" : None,
+            "address" : None,
+            "city" : None,
+            "latitude" : None,
+            "longitude" : None,
+            "fuelname" : None,
+            "fueltype" : fuel_type,
+            "date" : None
+        }
+
+        filterSet = False
+        if filter is not None and filter.strip() != "":
+            filterSet = filter.strip().lower()
+
+        for station in price_info:
+            if filterSet:
+                match = re.search(filterSet, station.get("brand").lower())
+                if not match:
+                    continue
+            try:
+                currDistance = float(station.get("distance"))
+                currPrice = float(station.get("price"))
+            except ValueError:
+                continue
+            if currDistance <= max_distance and (data.get("price") == None or currPrice < data.get("price")):
+                data["distance"] = float(station.get("distance"))
+                data["price"] = float(station.get("price"))
+                data["localPrice"] = 0 if price_info[0].get("price") == '' else float(price_info[0].get("price"))
+                data["diff"] = round(data["price"] - data["localPrice"],3)
+                data["diff30"] = round(data["diff"] * 30,3)
+                data["diffPct"] = round(100*((data["price"] - data["localPrice"])/data["price"]),3)
+                data["supplier"]  = station.get("name")
+                data["supplier_brand"]  = station.get("brand")
+                data["url"]   = station.get("url")
+                data["entity_picture"] = station.get("logo_url")
+                data["address"] = station.get("address")
+                data["city"] = station.get("locality")
+                data["latitude"] = station.get("lat")
+                data["longitude"] = station.get("lon")
+                data["fuelname"] = station.get("fuelname")
+                data["date"] = station.get("date")
+        if data["supplier"] is None and filterSet:
+            _LOGGER.warning(f"{NAME} {postalcode} the station filter '{filter}' may result in no results found, if needed, please review filter")
+        
+        _LOGGER.debug(f"{NAME} get_lowest_fuel_price info found: {data}")
+        hass.bus.async_fire(f"{DOMAIN}_lowest_fuel_price", data)
+        
+           
+
+
+    hass.services.async_register(DOMAIN, 'get_lowest_fuel_price', handle_get_lowest_fuel_price)
+    _LOGGER.info(f"async_register done")
